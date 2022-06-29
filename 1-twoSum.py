3eb1be2061e24412716167373107397e2327fc86
Angel <santoyox714@gmail.com>
2022-06-29 09:39:41 -0700

    Create 1-twoSum.py

---
+++ 1-twoSum.py
@@ -0,0 +1,10 @@
+ #Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+  
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i,n):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i,j]
+           
