db5ca98343bf77849fbe18c8bf0580329fb819d1
Angel <santoyox714@gmail.com>
2022-07-01 15:08:56 -0700

    Create ProdArrayExceptSelf.py

---
+++ ProdArrayExceptSelf.py
@@ -0,0 +1,24 @@
+#Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+#You must write an algorithm that runs in O(n) time and without using the division operation.
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:  
+        n=len(nums)
+        leftP=  [1]*n 
+        rightP= [1]*n
+        
+       
+        for i in range(1, n):               #leftProduct array
+            leftP[i] = leftP[i-1] * nums[i-1]
+
+        
+        for i in reversed(range(0,n-1)):    #rightProduct array
+            rightP[i] =rightP[i+1] * nums[i+1]
+            
+        # ans array by multiplying left and right arrays at each index
+        for i in range(n):
+            nums[i] = leftP[i]  *  rightP[i]
+            
+        return nums     
+    
+     
