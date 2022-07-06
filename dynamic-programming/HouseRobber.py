cf422fa719cb91343886e8e2790719d97958038f
Angel <santoyox714@gmail.com>
2022-07-06 10:38:56 -0700

    Update HouseRobber.py

--- dynamic-programming/HouseRobber.py
+++ dynamic-programming/HouseRobber.py
@@ -5,15 +5,14 @@ class Solution:
             last, curr = curr, max(last + i, curr)
         return curr
       
-     def rob2(self, nums: List[int]) -> int:  #another way to approach
+       
+    
+     def rob2(self, nums: List[int]) -> int:  #another approach
         #create arr
-        rob = [0] * (len(nums) + 1)
-        
+        rob = [0] * (len(nums) + 1)  
         # Base case: 0 rooms and 1 room
         rob[0] = 0
-        rob[1] = nums[0]
-        
-        
+        rob[1] = nums[0]     
         # Solving sub questions and store the sub solutions: more than 1 rooms
         for i in range(2, len(nums) + 1):
             # For any given number at index n: compare dp[n-2] + number and dp[n-1]
