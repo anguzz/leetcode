1fa7165396c2d24c385d42c1881fb55eb6034925
Angel <santoyox714@gmail.com>
2022-07-10 18:34:46 -0700

    Update 5-groupAnagrams.py

--- strings/5-groupAnagrams.py
+++ strings/5-groupAnagrams.py
@@ -1,14 +1,14 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = defaultdict(list)  #dic list to count anagrams
+        lookup = defaultdict(list)  #dic list to count anagrams
         
         for word in strs:            
-            frequencyList = [0]*26 
+            key = [0]*26 
             for char in word:       
-                frequencyList[ord(char) - ord('a')] += 1 
-            d[tuple(frequencyList)].append(word)
-        return d.values()
+                key[ord(char) - ord('a')] += 1 
+            lookup[tuple(key)].append(word)
+        return lookup.values()
                 
 #iterate through all our words
 #iterate through each letter of are words and get count
