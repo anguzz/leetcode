ccc4cc155d19d309b36120c370efc79059c08275
Angel <santoyox714@gmail.com>
2022-07-12 13:43:50 -0700

    Update 9-decodeWays.py

--- dynamic-programming/9-decodeWays.py
+++ dynamic-programming/9-decodeWays.py
@@ -13,10 +13,8 @@ class Solution:
         dp = [1] * len(s) #dp array filled with 1
         
         for i in range(1, len(s)): 
-            # One digit check
-            dp[i] =0  if int(s[i]) == 0 else dp[i - 1] #if index is zero fill dp with zero else return it with previous number before zero
-            
-            
+            dp[i] =0  if int(s[i]) == 0 else dp[i - 1] # One digit check and if index is zero fill dp[i] with zero else fill it with previous i
+           
             if 9 < int(s[i-1:i+1]) < 27:           # Two digit check and if after 1st index & between 10-26 and not at first index
                 dp[i] += dp[i - 2 if i > 1 else 0]  #add prevPrev index
        
