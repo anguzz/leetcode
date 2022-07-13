e65c4c22824ebcc594ef1ec6aee75720f9224ca0
Angel <santoyox714@gmail.com>
2022-07-13 13:24:30 -0700

    Delete 11-HouseRobber1.py

--- dynamic-programming/11-HouseRobber1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        prev= prevPrev=0
-        
-        for n in nums:
-            temp=max(n+prev, prevPrev)
-            prev= prevPrev
-            prevPrev = temp
-        return temp
