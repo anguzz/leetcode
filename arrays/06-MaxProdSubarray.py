941eb3f151d61f728e78e6ce03dc9af6a47a0129
Angel <santoyox714@gmail.com>
2022-11-02 20:23:55 -0700

    Fix MaxProdSubarray

--- arrays/06-MaxProdSubarray.py
+++ arrays/06-MaxProdSubarray.py
@@ -1,19 +1,36 @@
-
 #Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
-
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        mx = 1 
-        mn = 1
-        ans = max(nums)
+        res = maxP = minP = nums[0]
+        for n in nums[1:]:  
+            maxP, minP = max(n, maxP*n, minP*n), min(n, maxP*n, minP*n) #imporant this happens in single line so maxP/minP are not yet affected in max/min function
+            res = max(res, maxP)
+        return res
         
-        for n in nums:
-            mx = max(n, mx*n, mn*n)
-            mn = min(n, mx*n, mn*n)
-            ans= max(mx, mn, ans)
-        return ans
+        
+        #longer readable solution
+        ''' 
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        if len(nums)==0: #base case
+            return 0
+        
+        res = maxP = minP = nums[0]
+        
+        for i in range(1, len(nums)):
+            curr=nums[i]
+            tempMax = max(curr, maxP*curr, minP*curr)
+            minP    = min(curr, maxP*curr, minP*curr) 
+            maxP    = tempMax
+            res     = max(res, maxP)
             
-   #keep track of curr min and max due to the sign of the numbers
-   #we track the min value incase of negative value
- 
+        return res
+        '''
+    
+  
+    
+
+   
+   
+  
