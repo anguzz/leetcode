27a63bd8058c60d8ad42828fb1067e2981d59e5b
Angel <santoyox714@gmail.com>
2022-11-08 02:03:27 -0800

    Update 03-validPalindrome.py

--- strings/03-validPalindrome.py
+++ strings/03-validPalindrome.py
@@ -4,3 +4,17 @@ class Solution:
         return all(s2[i]==s2[~i] for i in range(int(len(s2)/2)))
          
            
+'''
+then compare using all function checking i with the complement of i, 
+all checks every item in list are true returns true if so, if not false
+
+how invert operator works in this case
+i  ~i
+-----
+0  -1
+1  -2
+2  -3
+3  -4 
+4  -5 
+5  -6
+'''
